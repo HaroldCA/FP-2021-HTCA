0753bebb90092a6d5bb0070e8e5e643d88246412
HaroldCA <>
2021-04-26 14:33:07 +0000

    “probando”

---
+++ AreaTriangulo.rb
@@ -0,0 +1,11 @@
+#Definir variables y otros
+puts "Ejemplo 01-Area de un Triangulo"
+#Dtaos de Entrada
+puts "Ingrese Base"
+b=gets.to_i
+puts "Ingrese Altura"
+b=gets.to_i
+#Proceso
+area=(b*h)/2
+#Datos de Salida
+puts "El area del triangulo es: #{area}"
